751bf115e8961c645d14300812f4d523e062a703
Hamza HArmouche <58759442+harmouche@users.noreply.github.com>
2021-03-13 20:45:11 -0500

    request 1 completed

--- request_1_pd.py
+++ request_1_pd.py
@@ -3,30 +3,48 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
-formName = input("Enter form name here: ").replace(' ', '+')
+ilist = ['Form W-2', 'Form 1095-C', 'Form 1099-MISC', 'Form W-2 P']
 
 
-url = f'https://apps.irs.gov/app/picklist/list/priorFormPublication.html?value={formName}&criteria=formNumber&submitSearch=Find'
+def irs_form(form_name):
 
-df = pd.read_html(url)[3]
-# print(df[3])
-# df_json = df[3] 
-# .to_json(f'{formName}.json')
-# print(df_json)
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
-table = soup.find_all('table')[3]
+    modi_form_name = form_name.replace(' ', '+')
 
-links = []
-for tr in table.findAll("tr"):
-    trs = tr.findAll("td")
-    for each in trs:
-        try:
-            link = each.find('a')['href']
-            links.append(link)
-        except:
-            pass
+    url = f'https://apps.irs.gov/app/picklist/list/priorFormPublication.html?resultsPerPage=200&sortColumn=sortOrder&indexOfFirstRow=0&criteria=formNumber&value={modi_form_name}&isDescending=false'
 
-df['Link'] = links
-print(df)
-df_json = df.to_json(f'{formName}.json')
+    df = pd.read_html(url)[3]
+
+
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    table = soup.find_all('table')[3]
+
+    links = []
+    for tr in table.findAll("tr"):
+        trs = tr.findAll("td")
+        for each in trs:
+            try:
+                link = each.find('a')['href']
+                links.append(link)
+            except:
+                pass
+
+    df['Link'] = links
+
+    df = df[df['Product Number'] == form_name]
+    new_result = {
+    "form_number": df.iloc[1,0],
+    "form_title": df.iloc[1,1],
+    "min_year": int(df['Revision Date'][df['Revision Date'].idxmin()]),
+    "max_year": int(df['Revision Date'][df['Revision Date'].idxmax()])}
+
+    output = json.dumps(new_result)
+    return output
+
+
+def irs_forms(ilist):
+    output_list = []
+    for element in ilist:
+        output_list.append(irs_form(element))
+    return output_list
+print(irs_forms(ilist))
